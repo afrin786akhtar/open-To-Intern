e26af1c609067c717bec928ca78f3d3084abaeb8
afrin786akhtar <afrin786akhtar@gmail.com>
2022-09-14 11:23:11 +0530

    College-Model

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+//-----------------dummy---------------------
 
 router.get("/test-me",function(req,res){
     res.send("maari api testing")
 })
+
+//--------------
 
 module.exports = router
