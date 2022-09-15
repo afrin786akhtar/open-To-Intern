e678c5d50a273d29a9becfb673406a75fb2c9dab
Harikesh9690 <harikesmtr02018@gmail.com>
2022-09-15 13:31:49 +0530

    finel

--- src/Controllers/InternController.js
+++ src/Controllers/InternController.js
@@ -1,23 +1,32 @@
 const CollegeModel = require('../models/CollegeModel');
 const InternModel = require('../models/InternModel')
 
+const isValid = function (value) {
+    if (typeof value === "undefined" || value === null) return false;
+    if (typeof value === "string" && value.trim().length === 0) return false;
+    if (typeof value === "object" && Object.keys(value).length === 0) return false;
+    return true;
+};
+
 const createIntern = async function (req, res) {
     try {
         let data = req.body;
         let alphabets = /^[A-Z][A-Z a-z]{3,20}$/
         let emailValid = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/
-        let mobileValid = /^([+]\d{2})?\d{10}$/
+        let mobileValid = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/
 
-        if (Object.keys(data).length === 0) {
+        if (!isValid(data)) {
             return res.status(400).send({ status: false, msg: "You have not provided any data" })
         }
-        if (!data.name) {
+        if (!isValid(data.name)) {
             return res.status(400).send({ status: false, msg: "Please provide name. it's mandatory" })
+        } else {
+            data.name = data.name.trim().split(" ").filter(word => word).join(" ")
         }
         if (!alphabets.test(data.name)) {
             return res.status(400).send({ status: false, msg: "name must contain only letters and first letter is capital" })
         }
-        if (!data.email) {
+        if (!isValid(data.email)) {
             return res.status(400).send({ status: false, msg: "Please provide email. it's mandatory" })
         }
         if (!emailValid.test(data.email)) {
@@ -27,21 +36,27 @@ const createIntern = async function (req, res) {
             return res.status(400).send({ status: false, msg: "Please provide Mobile Number. it's mandatory" })
         }
         if (!mobileValid.test(data.mobile)) {
-            return res.status(400).send({ status: false, msg: "please provide ten digit mobile Number" })
+            return res.status(400).send({ status: false, msg: "please provide valid mobile Number" })
         }
-        let student = await InternModel.findOne({ email: data.email })
-        if (student) {
-            if (student.email) {
+        if (!isValid(data.collegeName)) {
+            return res.status(400).send({ status: false, msg: "please provide college Name" })
+        }else{
+            data.collegeName = data.collegeName.trim()
+        }
+        let studentemail = await InternModel.findOne({ email: data.email })
+        if (studentemail) {
+            if (studentemail.email) {
                 return res.status(400).send({ status: false, msg: "this email is already exist" })
             }
         }
-        if (student) {
-            if (student.mobile) {
+        let studentdmobile = await InternModel.findOne({ mobile: data.mobile })
+        if (studentdmobile) {
+            if (studentdmobile.mobile) {
                 return res.status(400).send({ status: false, msg: "this mobile Number is already exist" })
             }
         }
 
-        const colleges = await CollegeModel.findOne({ name: data.collegeName }, { _id: 1 })
+        const colleges = await CollegeModel.findOne({ name: data.collegeName })
         if (!colleges) {
             return res.status(400).send({ status: false, msg: "this college is not available" })
         } else {
--- src/Controllers/collegeController.js
+++ src/Controllers/collegeController.js
@@ -1,23 +1,35 @@
 const collegeModel = require('../models/CollegeModel')
 const InternModel = require('../models/InternModel')
 
+
+const isValid = function (value) {
+    if (typeof value === "undefined" || value === null) return false;
+    if (typeof value === "string" && value.trim().length === 0) return false;
+    if (typeof value === "object" && Object.keys(value).length === 0) return false;
+    return true;
+};
+
 const createCollege = async function (req, res) {
     try {
         let data = req.body
-        if (Object.keys(data).length === 0) {
+        if (!isValid(data)) {
             return res.status(400).send({ status: false, msg: "You have not provided any data" })
         }
-        if (!data.name) {
+        if (!isValid(data.name)) {
             return res.status(400).send({ status: false, msg: "Please provide name. it's mandatory" })
+        } else {
+            data.name = data.name.trim().split(" ").filter(word => word).join(" ")
         }
         let college = await collegeModel.findOne({ name: data.name })
         if (college) {
-            return res.status(400).send({ status: false, msg: "this college name is already reserved" })
+            return res.status(409).send({ status: false, msg: "this college name is already reserved" })
         }
-        if (!data.fullName) {
+        if (!isValid(data.fullName)) {
             return res.status(400).send({ status: false, msg: "Please provide fullName. it's mandatory" })
+        } else {
+            data.fullName = data.fullName.trim().split(" ").filter(word => word).join(" ")
         }
-        if (!data.logoLink) {
+        if (!isValid(data.logoLink)) {
             return res.status(400).send({ status: false, msg: "Please provide logoLink. it's mandatory" })
         }
         let savedata = await collegeModel.create(data)
@@ -30,18 +42,33 @@ const createCollege = async function (req, res) {
 const getcollegeDetails = async function (req, res) {
     try {
         let collegeName = req.query.collegeName
-        let findcollege= {}
-             findcollege = await collegeModel.findOne({ name: collegeName },{updatedAt:0,createdAt:0,__v:0,isDeleted:0})
-        let studentdetails = await InternModel.find({ collegeId: findcollege._id },{updatedAt:0,createdAt:0,__v:0,collegeId:0,isDeleted:0})
+        if (!collegeName) {
+            return res.status(400).send({ status: false, msg: "please Enter college name" })
+        }
+        collegeName = collegeName.toLowerCase()
+        collegeName = collegeName.toUpperCase()
+        let findcollege = await collegeModel.findOne({ name: collegeName })
+        if (!findcollege) {
+            return res.status(404).send({ status: false, msg: "don't open Internships in this college" })
+        }
+        let studentdetails = await InternModel.find({ collegeId: findcollege._id }).select({ _id: 1, name: 1, email: 1, mobile: 1 })
+        if (studentdetails.length === 0) {
+            studentdetails.push("don't have any intern in this college")
+        }
 
-        // Object.assign(findcollege, {interns: studentdetails})
-        // collegeDetails.findcollege= findcollege
-        findcollege.interns = studentdetails
-        res.status(200).send({status: true, data: findcollege})
+        let data = {}
+        data.name = findcollege.name
+        data.fullName = findcollege.fullName
+        data.logoLink = findcollege.logoLink
+        data.interns = studentdetails
+
+        res.status(200).send({ status: true, data: data })
     } catch (err) {
         return res.status(500).send({ status: false, error: err.message })
     }
 }
 
+
 module.exports.getcollegeDetails = getcollegeDetails
 module.exports.createCollege = createCollege
+
